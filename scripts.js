e18936569a30225f8301992792dc6690d41a5719
Runali Phillips <rons_millz@yahoo.com>
2020-02-03 13:04:17 -0600

    Add argument

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Declare and assign variables
+//const studentName = "Runali";
+
+//const year = 2019;
+// We accessing an OBJECT called 'console' and using
+// DOT NOTATION to access a Key is a method, so we INVOKE
+// with ().
+// Arugement provide additional information that a METHOD TO DO ITS JOB.
+console.log("Hello World!");
