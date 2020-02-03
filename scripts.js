bb27992da054598f50319243e9ab78e80e16635f
Runali Phillips <rons_millz@yahoo.com>
2020-02-03 13:46:11 -0600

    Add examples of coercion

--- scripts.js
+++ scripts.js
@@ -10,4 +10,32 @@ const year = 2018;
 //console.log("My name is " + studentName + "been coding since" + year);
 
 // Template Literals string interpolation
-console.log(`My name is ${studentName}`);
+//console.log(`My name is ${studentName}`);
+
+// Dynamically Typed
+// let x = 23;
+// x = "No I am a string";
+// x = true;
+
+// Create a variable "timeOfDay" and assign it a string
+//const timeOfDay = "Morning";
+//console.log("Good " + timeOfDay);
+//console.log(`Good ${timeOfDay}`);
+
+//console.log(typeof timeOfDay);
+
+// JS will COERCE your data types into whatever it feels it appropriate
+// IMPLICIT COERCION
+// console.log("hello" + 22);
+// console.log(22 + 22);
+// console.log(22 + false);
+// console.log(22 + true);
+
+console.log("3" * 3);
+
+//  Explicit Coercion
+// console.log(Number(false));
+
+console.log(parseInt("123.45"));
+console.log(parseFloat("123.45"));
+console.log(Boolean(222)); // the big Boolean is a construction.. whatever not zero is true
