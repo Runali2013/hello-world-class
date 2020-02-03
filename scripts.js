404d1d7c2b4d72f456e2f9af512b85ad07636d01
Runali Phillips <rons_millz@yahoo.com>
2020-02-03 13:11:09 -0600

    Show concatatination

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 // Declare and assign variables
-//const studentName = "Runali";
+const studentName = "Runali";
 
-//const year = 2019;
+const year = 2018;
 // We accessing an OBJECT called 'console' and using
 // DOT NOTATION to access a Key is a method, so we INVOKE
 // with ().
 // Arugement provide additional information that a METHOD TO DO ITS JOB.
-console.log("Hello World!");
+// console.log("Hello World!");
+console.log("My name is " + studentName + "been coding since" + year);
